bbd07f04fc9d090c9e65b1bcda021ff0cb79dfb9
Evangelina Vega <evangelina1001@hotmail.com>
2024-07-23 16:31:05 -0700

    Finished exercise

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,28 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
+  // console.log(data1)
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    // console.log(data2)
+    fs.readFile("./age.txt", "utf-8", (err, data3) =>{
+      // console.log(data3)
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) =>{
+        // console.log(data4)
+        const array = data4.split("")
+        // console.log(array)
+        array.splice(0, 1)
+        array.splice(0, 1)
+        array.splice(8,1)
+        array.splice(8,1)
+        array.splice(9,1)
+        array.splice(16,1)
+        array.splice(16,1)
+        const array2 = array.join("")
+        // console.log(array2)
+        const array3 = array2.split(" ")
+        // console.log(array3)
+        console.log(`${data1} ${data2} is ${data3} years old and his hobbies are ${array3[0]} and ${array3[1]}.`)
+      })
+    })
+  })
 })
